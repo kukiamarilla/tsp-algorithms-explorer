9e208294fa2dc8f82214033b2cfd4b4418667473
Mateo Fidabel <mateofidabel@gmail.com>
2021-08-14 18:35:32 -0400

    Se cambia a bootstrap pq xd

--- src/App.js
+++ src/App.js
@@ -1,31 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {AppBar, createStyles, makeStyles, Toolbar, Typography} from "@material-ui/core";
-import Home from "./components/Home";
-
-const useStyles = makeStyles(() =>
-    createStyles({
-        root: {
-            flexGrow: 1
-        }
-    })
-);
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logoPoli from "./img/LogoFPUNA.jpeg"
+import {Col, Container, Form, Image, Row} from "react-bootstrap";
 
 function App() {
-    const classes = useStyles();
-
+    const [algoritmo, setAlgoritmo] = useState(0)
+    const [tamanho, setTamanho] = useState(1)
   return (
-      <div className={classes.root}>
-          <AppBar position="sticky">
-              <Toolbar>
-                  <Typography variant="h6">
-                      Traveling Salesman Problem
-                  </Typography>
-              </Toolbar>
-          </AppBar>
-          <Home/>
-      </div>
+      <>
+        <Container fluid className="m-0">
+            <Row>
+                <Col sm={6}>
+                    <Image src={logoPoli} alt="FPUNA" className="logo"/>
+                    <h1>Traveling Salesman Problem</h1>
+                    <h2>Integrantes</h2>
+                    <li>Mateo Fidabel</li>
+                    <li>Santiago Acevedo</li>
+                    <li>Angel Ferreira</li>
+                    <li>Isaac Amarilla</li>
+                </Col>
+                <Col sm={6}>
+                    <Form className="mt-5">
+                        <Form.Group>
+                            <Form.Label>Algoritmo</Form.Label>
+                            <Form.Select value={algoritmo}
+                                         onChange={event => {setAlgoritmo(event.target.value)}}>
+                                <option value={0}>Seleccione el algoritmo</option>
+                                <option value={1}>Backtracking</option>
+                                <option value={2}>Algoritmo de las vegas</option>
+                                <option value={3}>Avaro + 2-opt local</option>
+                            </Form.Select>
+                        </Form.Group>
+                        <br/>
+                        <Form.Group>
+                            <Form.Label>Valor de N</Form.Label>
+                            <Form.Control type="number"
+                                          value={tamanho}
+                                          onChange={event => {setTamanho(event.target.value)}}/>
+                        </Form.Group>
+                    </Form>
+                </Col>
+            </Row>
+            <Row>
+
+            </Row>
+        </Container>
+      </>
   );
 }
 
