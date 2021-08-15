d92e188932dd7ee07956a4ece98ee3f76b1f107a
kukiamarilla <sc.amarilla@gmail.com>
2021-08-15 18:47:16 +0000

    Deploying to gh-pages from @ kukiamarilla/tsp-algorithms-explorer@fcc5e9fce630a20e13b74d8e6dfe8e90d333c780 🚀

--- src/funciones.js
+++
@@ -1,8 +0,0 @@
-
-export const crearMatriz = (n) => {
-    return Array(n).fill().map( () => Array(n).fill(0))
-}
-
-export const clonarMatriz = (matriz) => {
-    return JSON.parse(JSON.stringify(matriz))
-}
